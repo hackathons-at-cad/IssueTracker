9cbdd8b10f2ea11e5d94cdcd9af7f327966c9287
sophi-e <sophierithous@gmail.com>
2020-09-19 00:30:30 +0100

    created login and register pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,65 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import FormContainer from "../components/formcontainer";
+import styled from "@emotion/styled";
+
+export const StyledFormWrapper = styled.div`
+  width: 350px;
+  margin: 12em auto;
+  background: #ffffff;
+  border: 1px solid #dfdfdf;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.06);
+  border-radius: 6px;
+  padding: 2em 1em;
+
+  .head-text {
+    display: flex;
+    justify-content: center;
+    align-item: center;
+    vertical-align: middle;
+  }
+  .box {
+    background: #673ab7;
+    height: 30px;
+    width: 30px;
+    display: inline-block;
+    margin-right: 10px;
+  }
+
+  p {
+    text-align: center;
+  }
+`;
 
 const Login = () => {
   return (
-    <div>login</div>
-    // <FormContainer>
-    //   <Form>
-    //     <Form.Group controlId="formBasicEmail">
-    //       <Form.Label>Email address</Form.Label>
-    //       <Form.Control type="email" placeholder="Enter email" />
-    //       <Form.Text className="text-muted">
-    //         We'll never share your email with anyone else.
-    //       </Form.Text>
-    //     </Form.Group>
+    <StyledFormWrapper>
+      <p className="head-text">
+        {" "}
+        <div className="box"></div>Welcome Back
+      </p>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Input value" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-    //     <Form.Group controlId="formBasicPassword">
-    //       <Form.Label>Password</Form.Label>
-    //       <Form.Control type="password" placeholder="Password" />
-    //     </Form.Group>
-    //     <Form.Group controlId="formBasicCheckbox">
-    //       <Form.Check type="checkbox" label="Check me out" />
-    //     </Form.Group>
-    //     <Button variant="primary" type="submit">
-    //       Submit
-    //     </Button>
-    //   </Form>
-    // </FormContainer>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Input value" />
+        </Form.Group>
+        <Button variant="primary" type="submit" block>
+          Log In
+        </Button>
+        <p>
+          <a href="/register">Create an account</a>
+        </p>
+      </Form>
+    </StyledFormWrapper>
   );
 };
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { StyledFormWrapper } from "./Login";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <StyledFormWrapper>
+      <p className="head-text">
+        {" "}
+        <div className="box"></div>Create Account
+      </p>
+      <Form>
+        <Form.Group controlId="formText">
+          <Form.Label>FirstName</Form.Label>
+          <Form.Control type="text" placeholder="Input value" />
+        </Form.Group>
+        <Form.Group controlId="formText">
+          <Form.Label>Surname</Form.Label>
+          <Form.Control type="text" placeholder="Input value" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Input value" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Input value" />
+        </Form.Group>
+        <Button variant="primary" type="submit" block>
+          Log In
+        </Button>
+        <p>
+          <a href="/register">Create an account</a>
+        </p>
+      </Form>
+    </StyledFormWrapper>
+  );
 };
 
 export default Register;
